97a80af71b58a1dbb2e0215a756435a94eb4cd67
KKamJi <xowl5460@naver.com>
2024-11-13 21:08:09 +0900

    add aws_region and add pgp_key variable

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 ###############################################################
 ##                      Parameter Store                      ##
 ###############################################################
@@ -7,33 +11,14 @@ data "aws_ssm_parameter" "pgp_key" {
 
 ########################IAM User Group#########################
 # IAM User, User Group 생성
-# module "dev_group" {
-#   source          = "./modules/iam_group_membership"
-#   group_name      = "dev-group"
-#   membership_name = "dev-group-membership"
-#   user_names      = ["ycs"]
-#   user_path       = "/dev/"
-#   pgp_key         = var.pgp_key
-#   policy_file     = "${path.module}/template/dev-policy.json"
-# }
-
-# module "infra_group" {
-#   source          = "./modules/iam_group_membership"
-#   group_name      = "infra-group"
-#   membership_name = "infra-group-membership"
-#   user_names      = ["ktj", "cbh"]
-#   user_path       = "/infra/"
-#   pgp_key         = var.pgp_key
-#   policy_file     = "${path.module}/template/infra-policy.json"
-# }
 module "dev_group" {
   source          = "./modules/iam_group_membership"
   group_name      = "dev-group"
   membership_name = "dev-group-membership"
   user_names      = ["ycs"]
   user_path       = "/dev/"
-  pgp_key         = data.aws_ssm_parameter.pgp_key.value
-  policy_file     = "${path.module}/templates/dev-policy.json"
+  pgp_key         = var.pgp_key
+  policy_file     = "${path.module}/template/dev-policy.json"
 }
 
 module "infra_group" {
@@ -42,6 +27,25 @@ module "infra_group" {
   membership_name = "infra-group-membership"
   user_names      = ["ktj", "cbh"]
   user_path       = "/infra/"
-  pgp_key         = data.aws_ssm_parameter.pgp_key.value
-  policy_file     = "${path.module}/templates/infra-policy.json"
+  pgp_key         = var.pgp_key
+  policy_file     = "${path.module}/template/infra-policy.json"
 }
+# module "dev_group" {
+#   source          = "./modules/iam_group_membership"
+#   group_name      = "dev-group"
+#   membership_name = "dev-group-membership"
+#   user_names      = ["ycs"]
+#   user_path       = "/dev/"
+#   pgp_key         = data.aws_ssm_parameter.pgp_key.value
+#   policy_file     = "${path.module}/templates/dev-policy.json"
+# }
+
+# module "infra_group" {
+#   source          = "./modules/iam_group_membership"
+#   group_name      = "infra-group"
+#   membership_name = "infra-group-membership"
+#   user_names      = ["ktj", "cbh"]
+#   user_path       = "/infra/"
+#   pgp_key         = data.aws_ssm_parameter.pgp_key.value
+#   policy_file     = "${path.module}/templates/infra-policy.json"
+# }
