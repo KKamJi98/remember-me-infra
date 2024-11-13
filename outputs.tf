5f2c2f5e12c1ae05fa90adac4361984ac798e99b
KKamJi <xowl5460@naver.com>
2024-11-13 20:50:54 +0900

    add iam_group_membership

---
+++ outputs.tf
@@ -0,0 +1,27 @@
+###############################################################
+##             [module] - iam_group_membership               ##
+###############################################################
+
+output "dev_group_name" {
+  value = module.dev_group.group_name
+}
+
+output "infra_group_name" {
+  value = module.infra_group.group_name
+}
+
+output "dev_group_user_names" {
+  value = module.dev_group.user_names
+}
+
+output "infra_group_user_names" {
+  value = module.infra_group.user_names
+}
+
+output "dev_group_encrypted_passwords" {
+  value = module.dev_group.formatted_decrypted_passwords
+}
+
+output "infra_group_encrypted_passwords" {
+  value = module.infra_group.formatted_decrypted_passwords
+}
