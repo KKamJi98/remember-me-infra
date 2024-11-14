89e6b5f453cb1a862212a7d72790d74a5f233480
KKamJi <xowl5460@naver.com>
2024-11-14 16:10:48 +0900

    update code format

--- main.tf
+++ main.tf
@@ -3,14 +3,9 @@ provider "aws" {
 }
 
 ###############################################################
-##                      Parameter Store                      ##
+## iam_group_membership
 ###############################################################
-data "aws_ssm_parameter" "pgp_key" {
-  name = "/terraform/keybase/pgp_key"
-}
 
-########################IAM User Group#########################
-# IAM User, User Group 생성
 module "dev_group" {
   source          = "./modules/iam_group_membership"
   group_name      = "dev-group"
@@ -30,22 +25,3 @@ module "infra_group" {
   pgp_key         = var.pgp_key
   policy_file     = "${path.module}/templates/infra-policy.json"
 }
-# module "dev_group" {
-#   source          = "./modules/iam_group_membership"
-#   group_name      = "dev-group"
-#   membership_name = "dev-group-membership"
-#   user_names      = ["ycs"]
-#   user_path       = "/dev/"
-#   pgp_key         = data.aws_ssm_parameter.pgp_key.value
-#   policy_file     = "${path.module}/templates/dev-policy.json"
-# }
-
-# module "infra_group" {
-#   source          = "./modules/iam_group_membership"
-#   group_name      = "infra-group"
-#   membership_name = "infra-group-membership"
-#   user_names      = ["ktj", "cbh"]
-#   user_path       = "/infra/"
-#   pgp_key         = data.aws_ssm_parameter.pgp_key.value
-#   policy_file     = "${path.module}/templates/infra-policy.json"
-# }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,13 @@
-variable "pgp_key" {
-  description = "Provide a base-64 encoded PGP public key for dev, or a keybase username in the form `keybase:username`. Required to encrypt password."
+variable "region" {
+  description = "AWS region to deploy to"
   type        = string
 }
 
-variable "region" {
-  description = "AWS region to deploy to"
+###############################################################
+## iam_group_membership
+###############################################################
+
+variable "pgp_key" {
+  description = "Provide a base-64 encoded PGP public key for dev, or a keybase username in the form `keybase:username`. Required to encrypt password."
   type        = string
 }
