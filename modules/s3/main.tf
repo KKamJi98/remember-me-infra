caedafd97704691e8f750a174dc66b88636ffb86
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2024-11-17 00:07:21 +0900

    ✏️ chore: Modify name
    Related to: #30

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "deploy_bucket" {
+resource "aws_s3_bucket" "example" {
   bucket        = var.name
   force_destroy = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.deploy_bucket.id
+  bucket = aws_s3_bucket.example.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.deploy_bucket.id
+  bucket = aws_s3_bucket.example.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -25,24 +25,24 @@ resource "aws_s3_bucket_acl" "example" {
     aws_s3_bucket_public_access_block.example,
   ]
 
-  bucket = aws_s3_bucket.deploy_bucket.id
+  bucket = aws_s3_bucket.example.id
   acl    = "public-read"
 }
 
 locals {
   policy_content  = file(var.policy_file)
-  replaced_policy = replace(local.policy_content, "BUCKET_ARN", aws_s3_bucket.deploy_bucket.arn)
+  replaced_policy = replace(local.policy_content, "BUCKET_ARN", aws_s3_bucket.example.arn)
 }
 
 resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.deploy_bucket.id
+  bucket = aws_s3_bucket.example.id
   policy = local.replaced_policy
 
   depends_on = [aws_s3_bucket_acl.example]
 }
 
-resource "aws_s3_bucket_website_configuration" "website-config" {
-  bucket = aws_s3_bucket.deploy_bucket.id
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
   index_document {
     suffix = "index.html"
   }
--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,24 +1,24 @@
 output "bucket_name" {
   description = "The name of the S3 bucket."
-  value       = aws_s3_bucket.deploy_bucket.bucket
+  value       = aws_s3_bucket.example.bucket
 }
 
 output "bucket_arn" {
   description = "The ARN of the S3 bucket."
-  value       = aws_s3_bucket.deploy_bucket.arn
+  value       = aws_s3_bucket.example.arn
 }
 
 output "website_endpoint" {
   description = "The website endpoint for the S3 bucket."
-  value       = aws_s3_bucket_website_configuration.website-config.website_endpoint
+  value       = aws_s3_bucket_website_configuration.example.website_endpoint
 }
 
 output "id" {
   description = "The Id of the S3 bucket."
-  value       = aws_s3_bucket.deploy_bucket.id
+  value       = aws_s3_bucket.example.id
 }
 
 output "domain_name" {
   description = "The Domain name of the S3 bucket."
-  value       = aws_s3_bucket.deploy_bucket.bucket_domain_name
+  value       = aws_s3_bucket.example.bucket_domain_name
 }
