2ab5139be873bb4bd9bdc9f6b642fcd0598b66ab
KKamJi <xowl5460@naver.com>
2024-11-18 19:21:15 +0900

    :bug:fix: change route53 provider using provider alias

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.75"
+    }
+  }
+}
+
 data "aws_route53_zone" "example" {
   name         = var.domain_name
   private_zone = false
