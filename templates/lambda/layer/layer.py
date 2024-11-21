8d167f00421200ac3a355a99a63cfdce3ec257a4
KKamJi <xowl5460@naver.com>
2024-11-21 22:42:38 +0900

    :sparkles:feat: add subscription_filter lambda layer

--- templates/lambda/layer/layer.py
+++ templates/lambda/layer/layer.py
@@ -1,8 +1,6 @@
 # lambda_function.py
 def lambda_handler(event, context):
-    # Call the function from the layer
-    message = hello_world()
     return {
         "statusCode": 200,
-        "body": message
+        "body": "hello world"
     }
