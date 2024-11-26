4cef50bcb7cacdffd412e612cafdb0d5b997745a
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2024-11-26 16:10:37 +0900

    🐛 fix: waf alarm
    dimension에 rule이 잡히지 않아서 콘솔에 보이는 항목들로 namespace를 수정해보았음

--- modules/waf/variables.tf
+++ modules/waf/variables.tf
@@ -2,8 +2,3 @@ variable "policy_file" {
   type        = string
   description = "Path to JSON file for the SNS-topic Policy"
 }
-
-variable "waf_rule" {
-  type        = string
-  description = "Rule of the WAF"
-}
